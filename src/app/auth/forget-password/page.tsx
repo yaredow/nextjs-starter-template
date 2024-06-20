f1e28fae0238b9eaed8df7aff41bfc1479caf156
yaredow <yaredyilma11@gmail.com>
2024-06-20 13:20:26 +0300

    add a forget password form

--- src/app/auth/forget-password/page.tsx
+++ src/app/auth/forget-password/page.tsx
@@ -1,1 +1,9 @@
-export default function Page() {}
+import ForgetPasswordForm from "@/components/form/forget-passowrd-form";
+
+export const metadata = {
+  title: "Forget Password",
+};
+
+export default function Page() {
+  return <ForgetPasswordForm />;
+}
