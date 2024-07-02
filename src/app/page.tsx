242592bc05f79d2eacb8f7d4cf27ba6609965bc9
yaredow <yaredyilma11@gmail.com>
2024-07-02 12:45:28 +0300

    feat: add a key features section to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Heros from "@/components/landing-page/hero";
+import KeyFeatures from "@/components/landing-page/key-features";
+import { Separator } from "@/components/ui/separator";
 
 export default function Home() {
   return (
     <div>
       <Heros />
+      <Separator />
+      <KeyFeatures />
     </div>
   );
 }
--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -18,6 +18,7 @@ export default function Heros() {
             reusable components, and a set of pre-styled elements to help you
             quickly create an engaging user interface.
           </p>
+
           <Link
             href="#"
             className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
